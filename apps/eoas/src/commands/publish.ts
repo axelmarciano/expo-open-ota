f19fd0b724250c0a6f0380e97892e3deddb891a0
Ubuntu <xzgredek@gmail.com>
2025-10-27 15:42:17 +0000

    feat(eoas-publish): add support for --platform argument when running expo export

--- apps/eoas/src/commands/publish.ts
+++ apps/eoas/src/commands/publish.ts
@@ -196,7 +196,8 @@ export default class Publish extends Command {
     const exportSpinner = ora('📦 Exporting project files...').start();
     try {
       await spawnAsync('rm', ['-rf', outputDir], { cwd: projectDir });
-      const { stdout } = await spawnAsync('npx', ['expo', 'export', '--output-dir', outputDir], {
+      const specifiedPlatform = platform === 'all' ? [] : ['--platform', platform];
+      const { stdout } = await spawnAsync('npx', ['expo', 'export', '--output-dir', outputDir, ...specifiedPlatform], {
         cwd: projectDir,
         env: {
           ...process.env,
