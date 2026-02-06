1ce930ae8aa63c0f9b2ec07e92efa7799f0255c3
Axel Marciano <axel.marciano@gmail.com>
2026-02-07 00:46:25 +0100

    feat: update docs

--- apps/docs/src/pages/index.tsx
+++ apps/docs/src/pages/index.tsx
@@ -19,7 +19,7 @@ function HomepageHeader() {
                 </Heading>
                 <p className={`hero__subtitle ${styles.heroSubtitle}`}>{siteConfig.tagline}</p>
                 <div className={styles.buttons}>
-                <Link className="button button--secondary button--lg" to="/docs/introduction">
+                <Link className="button button--secondary button--lg" to="/docs/getting-started/introduction">
                     Get started
                 </Link>
             </div>
