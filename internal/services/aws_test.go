75729afa671c0fd5df0b3e5c7acb9a05423e23bf
PeenDev <peendevproductions@gmail.com>
2026-05-04 12:14:15 -0600

    Added support for S3 Path styles instead of virtual host (#55)
    Co-authored-by: jay <xxxbrizzelxxx@gmail.com>

---
+++ internal/services/aws_test.go
@@ -0,0 +1,27 @@
+package services
+
+import (
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestApplyS3ClientOptionsUsesPathStyleWhenEnabled(t *testing.T) {
+	os.Setenv("AWS_S3_FORCE_PATH_STYLE", "true")
+	defer os.Unsetenv("AWS_S3_FORCE_PATH_STYLE")
+
+	options := s3.Options{}
+	applyS3ClientOptions(&options)
+
+	assert.True(t, options.UsePathStyle)
+}
+
+func TestApplyS3ClientOptionsUsesVirtualHostStyleByDefault(t *testing.T) {
+	os.Unsetenv("AWS_S3_FORCE_PATH_STYLE")
+
+	options := s3.Options{}
+	applyS3ClientOptions(&options)
+
+	assert.False(t, options.UsePathStyle)
+}
